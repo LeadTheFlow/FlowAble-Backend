a275cf94f2cb8bff33218e667b46780b610cf7ea
221B0825 <eunseoyu0825@gmail.com>
2023-12-11 20:03:22 +0900

    feat: fileUpload

---
+++ fileUpload.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, session
+from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('HELLO WORLD')
+
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app, resources={r"/api/*":{"origins":"*"}})
+CORS(app, expose_headers='Authorization')
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    target=os.path.join(UPLOAD_FOLDER,'test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    logger.info("welcome to upload`")
+    file = request.files['file'] 
+    filename = secure_filename(file.filename)
+    destination="/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath']=destination
+    response="Whatever you wish too return"
+    return response
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.run(debug=True,host="0.0.0.0",use_reloader=False, port=5003)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import subprocess
+
+if __name__ == '__main__':
+    subprocess.Popen(['python', 'apiTest.py'])
+    subprocess.Popen(['python', 'fileUpload.py'])
+    
